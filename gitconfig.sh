58f01ad0d54905695887ab2c3767befbd31321ce
Chad Homan <choman@gmail.com>
2016-09-14 13:58:50 -0500

    updated script

--- gitconfig.sh
+++ gitconfig.sh
@@ -4,7 +4,8 @@
 NAME="Chad Homan"
 EMAIL=choman@gmail.com
 
-git config --global core.editor vim
+git config --global core.editor vi
+git config --global push.default simple
 git config --local user.name "$NAME"
 git config --local user.email "$EMAIL"
 git config --local credential.helper cache
